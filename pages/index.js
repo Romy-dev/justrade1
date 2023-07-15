acb5c63efc64db4e47c657f0292823e76fdec099
Romy-dev <131360106+Romy-dev@users.noreply.github.com>
2023-07-15 17:15:26 +0000

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+
+const ratio = .1
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+};
+
+const handleInterset = function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.intersectionRatio > ratio) {
+         entry.target.classList.add('apparut-visible');
+         observer.unobserve(entry.target);
+    }
+   });
+};
+const observer = new IntersectionObserver(handleInterset, options) ;
+    document.querySelectorAll('[class*="apparut-"]').forEach(function (r) {
+    observer.observe(r);
+   });
+ window.addEventListener('scroll', function() {
+    var backToTopButton = document.getElementById('back-to-top');
+    if (window.pageYOffset > 200) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+  
+ document.getElementById('back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  const boxe = document.querySelector('.boxe');
+  const animatedText = boxe.querySelector('.animated-text');
+  
+  boxe.addEventListener('mouseenter', () => {
+    animatedText.style.animation = 'showText 1s forwards';
+  });
+  
+  boxe.addEventListener('mouseleave', () => {
+    animatedText.style.animation = '';
+  });
+  
+  
+  
